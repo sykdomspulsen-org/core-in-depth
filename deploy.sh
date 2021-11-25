306aab61c974bae578d19de99acba24ed9328e2b
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-11-25 06:57:18 +0100

    big changes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,10 @@
 
 Rscript -e "rmarkdown::render_site('.')"
 mv build_dir/output docs
+sed -s -i 's/<a href="index.html" title="">.*/<a href="https:\/\/docs.sykdomspulsen.no" title="">docs.sykdomspulsen.no<\/a>/g' docs/*html
+sed -s -i 's/font-size: 18px/font-size: 14px/g' docs/libs/bs4_book-1.0.0/bs4_book.css
 git add docs && git commit -m "Initial docs subtree commit"
-git subtree push --prefix docs origin gh-pages
+git push origin `git subtree split --prefix docs main`:gh-pages --force
 git reset --soft HEAD^
+git restore --staged .
 rm -rf docs
