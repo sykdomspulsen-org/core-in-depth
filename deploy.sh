6d59e93a35780fb357fd762df3e7216097ef3abc
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-03-01 11:54:23 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+reset
+
 Rscript -e "rmarkdown::render_site('.')"
-mv build_dir/output docs
-sed -s -i 's/<a href="index.html" title="">.*/<a href="https:\/\/docs.sykdomspulsen.no" title="">docs.sykdomspulsen.no<\/a>/g' docs/*html
-sed -s -i 's/font-size: 18px/font-size: 14px/g' docs/libs/bs4_book-1.0.0/bs4_book.css
-git add docs && git commit -m "Initial docs subtree commit"
-git push origin `git subtree split --prefix docs main`:gh-pages --force
-git reset --soft HEAD^
-git restore --staged .
-rm -rf docs
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+  mv build_dir/output docs
+
+  # https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
+  touch docs/.nojekyll
+
+  sed -s -i 's/<a href="index.html" title="">.*/<a href="https:\/\/docs.sykdomspulsen.no" title="">docs.sykdomspulsen.no<\/a>/g' docs/*html
+  sed -s -i 's/font-size: 18px/font-size: 14px/g' docs/libs/bs4_book-1.0.0/bs4_book.css
+  git add docs && git commit -m "Initial docs subtree commit"
+  git push origin `git subtree split --prefix docs main`:gh-pages --force
+  git reset --soft HEAD^
+  git restore --staged .
+  rm -rf docs
+else
+  echo "FAILED"
+fi
